65977689a5a00b70f06504bcc69eab53b5ea2be6
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2026-01-25 11:22:58 +0100

    Fix keyboard event

--- assets/js/events/keyboard_event.mjs
+++ assets/js/events/keyboard_event.mjs
@@ -5,13 +5,13 @@ import Type from "../type.mjs";
 export default class KeyboardEvent {
   static buildOperationParam(event) {
     return Type.map([
-      [Type.atom("alt_key"), Type.atom(event.altKey)],
-      [Type.atom("code"), Type.atom(event.code)],
-      [Type.atom("ctrl_key"), Type.atom(event.ctrlKey)],
-      [Type.atom("key"), Type.atom(event.key)],
-      [Type.atom("meta_key"), Type.atom(event.metaKey)],
-      [Type.atom("repeat"), Type.atom(event.repeat)],
-      [Type.atom("shift_key"), Type.atom(event.metaKey)],
+      [Type.atom("alt_key"), Type.boolean(event.altKey)],
+      [Type.atom("code"), Type.string(event.code)],
+      [Type.atom("ctrl_key"), Type.boolean(event.ctrlKey)],
+      [Type.atom("key"), Type.string(event.key)],
+      [Type.atom("meta_key"), Type.boolean(event.metaKey)],
+      [Type.atom("repeat"), Type.boolean(event.repeat)],
+      [Type.atom("shift_key"), Type.boolean(event.metaKey)],
     ]);
   }
 
